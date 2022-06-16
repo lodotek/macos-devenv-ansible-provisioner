c03461130a70f5b12f152834e59e31c2e2224a56
Christopher Montes <2421856+lodotek@users.noreply.github.com>
2022-06-16 17:04:43 -0600

    fix m1 support for GOROOT

--- roles/mac-dev-playbook/files/.zshrc.d/golang.sh
+++ roles/mac-dev-playbook/files/.zshrc.d/golang.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env zsh
 
 export PATH=$PATH:~/go/bin
-export GOROOT=/usr/local/opt/go/libexec
 export GOPATH=~/go/
 export GO111MODULE=on
+
+
+if [[ $OSTYPE == darwin* && $CPUTYPE == arm64 ]]; then
+  export GOROOT=/opt/homebrew/opt/go/libexec
+else
+  export GOROOT=/usr/local/opt/go/libexec
+fi
+
 
 alias got="go test"
 alias gotv="go test -cover"
